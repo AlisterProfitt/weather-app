a16cff090b3ad101d98157159a65843ffcb1a598
AlisterProfitt <alisterprofitt@gmail.com>
2024-11-16 21:17:41 -0400

    clean up syntax

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,11 +1,10 @@
 const KEY = "4WB2XHP2B4BTABGTS8YCNGX45";
 const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-export async function getWeatherDataByLocation(location) {
+async function getWeatherDataByLocation(location) {
     try {
-        const response = await fetch(`${URL}${location}?key=${KEY}`)
+        const response = await fetch(`${URL}${location}?key=${KEY}`);
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (exception) {
         console.log('Error! ' + exception);
@@ -16,7 +15,8 @@ export async function getRelevantWeatherData(location) {
     const data = await getWeatherDataByLocation(location);
     const week = {};
     for (let i = 0; i < 7; i += 1) {
-        let { temp: temperature, precipitation: rain, description, humidity, icon} = data.days[i];
+        let { temp: temperature, precipitation: rain, description, humidity, icon } = data.days[i];
         week["day" + i] = { temperature, rain, description, humidity, icon };
     }
+    return week;
 }
